32cca2f5dd895c07c61f1208e877043f7808aa3a
Devourd <32299889+Devourd@users.noreply.github.com>
2019-05-21 22:30:24 +0800

    code

---
+++ _code/Code_assignment/pojo_Guy/CommentData.java
@@ -0,0 +1,19 @@
+﻿package com.ldu.pojo;
+/**
+ * Commodity development joint query
+ * @author lyq
+ *
+ */
+import java.util.List;
+
+public class CommentData extends Goods{
+    private List<Comments> comments;
+	public List<Comments> getComments() {
+		return comments;
+	}
+	public void setComments(List<Comments> comments) {
+		this.comments = comments;
+	}
+	
+	
+}
